5c4048d233373504d8a21a659dc0ae1dae7695c2
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-11-06 09:52:48 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("Test GUI Program")
+
+
+
+
+
+
+window.mainloop()  #deixar esta linha sempre no fim do programa.... para manter a janela aberta como em uma espécie de while loop até que... 
